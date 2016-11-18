a209e032bd4ccfc9991eaf82938b0fa4f81615a7
Diogo Silva <diogo@diogok.net>
2016-11-18 13:07:27 -0200

    initial shp writing support

--- src/geof/writers.clj
+++ src/geof/writers.clj
@@ -5,6 +5,9 @@
   (:require msgpack.clojure-extensions)
 
   (:import [java.util.zip GZIPOutputStream])
+  (:import [org.geotools.data.shapefile ShapefileDumper])
+
+  (:require [geof.geo :as geo])
 
   (:require [clojure.java.io :as io])
   (:require [clojure.data.json :as json]))
@@ -37,15 +40,24 @@
   [writer & datasets]
   (msg/pack-stream (apply as-one datasets) writer))
 
-
 (defn write-to-shp
-  [output & datasets] nil)
+  [output & datasets] 
+  (let [all-feats (:features (apply as-one datasets))
+        by-type (group-by #(get-in % [:geometry :type]) all-feats)]
+    (if-not (.exists output) (.mkdir output))
+    (doseq [f-type by-type]
+      (let [re-output (io/file (str (.getAbsolutePath output ) "/" (key f-type) ))]
+        (if-not (.exists re-output) (.mkdir re-output))
+        (let [dumper (ShapefileDumper. re-output)]
+          (.dump dumper
+            (geo/make-feat-collection {:features (val f-type)})))))))
 
 (defn writer-for-1
   [output-file]
-  (if (re-find #"^-" output-file)
-    (io/output-stream System/out)
-    (io/output-stream (io/file output-file))))
+  (cond 
+    (re-find #"^-" output-file) (io/output-stream System/out)
+    (re-find #"shp$" output-file) (io/file output-file)
+    :else (io/output-stream (io/file output-file))))
 
 (defn writer-for-0
   [output-file]
@@ -60,14 +72,15 @@
     (re-find #"topo.?m(sg)?pack(.gz)?$" output-file) write-to-topo-msg-pack
     (re-find #"json(.gz)?$" output-file) write-to-geojson
     (re-find #"m(sg)?pack(.gz)?$" output-file) write-to-geo-msg-pack
-    (re-find #"shp$" output-file) write-to-shp
-    (re-find #"zip$" output-file) write-to-shp
-    :else nil))
+    :else write-to-shp))
 
 (defn writer-for
   [output-file] 
   (if-let [fun (writer-fn output-file)]
     (fn [& datasets]
-      (with-open [writer (writer-for-0 output-file)]
-        (apply fun writer datasets)))))
+      (if (= write-to-shp fun)
+        (apply fun (writer-for-0 output-file) datasets)
+        (with-open [writer (writer-for-0 output-file)]
+          (apply fun writer datasets)
+          (.flush writer))))))
 
